bed0b7d063545c724e6e663bba443c5078000e7f
Nxlfe2003@ <omar.ignammas@uit.ac.ma>
2024-07-20 16:29:32 +0100

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,15 @@
+    const express = require('express');
+    const app = express();
+
+    const cors = require('cors');
+
+
+   app.get('/',(req,res) => {
+    return res.json(("Hi there ....."))
+
+   })
+
+
+
+    app.listen(4000 ,() => console.log('listening on port 4000'));
+
